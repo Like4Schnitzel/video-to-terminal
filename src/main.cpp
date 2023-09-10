bfdd782f3565eeb1f4ee96f6de70b19d2561d514
Schnitzel <rw21062006@gmail.com>
2023-09-10 14:17:15 +0200

    enter videoPath as argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
     string videoPath;
-    cout << "Please enter the path to the video you want to play: ";
-    cin >> videoPath;
+    if (argc > 1)
+    {
+        videoPath = argv[1];
+    }
+    else 
+    {
+        cout << "Please enter the path to the video you want to play: ";
+        cin >> videoPath;
+    }
 
     VideoTranscoder trans = VideoTranscoder(videoPath);
     trans.transCodeFile();
