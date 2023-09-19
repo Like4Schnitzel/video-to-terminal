20f4052e8f078590dabda9173552d07e87772cd2
Like4Schnitzel <rw21062006@gmail.com>
2023-09-19 14:01:19 +0200

    write subArray method

--- src/VariousUtils.hpp
+++ src/VariousUtils.hpp
@@ -9,4 +9,17 @@ class VariousUtils {
         static int rfind(std::string str, char c);
         static int stringToInt(std::string s);
         static bool fileExists(std::string fileName);
+
+        template <typename T>
+        static T* subArray(const T* initArr, const int indexStart, const int indexEnd)
+        {
+            T* sub = (T*)malloc((indexEnd-indexStart + 1) * sizeof(T));
+            int subIndex = 0;
+            for (int i = indexStart; i <= indexEnd; i++)
+            {
+                sub[subIndex] = initArr[indexStart];
+                subIndex++;
+            }
+            return sub;
+        }
 };
